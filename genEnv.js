d0f13c66cb69aa50f0539c75f6745cbd5b5dada8
Ethan Arrowood <ethan.arrowood@gmail.com>
2017-10-18 00:50:20 -0400

    generate environment variables file added

---
+++ genEnv.js
@@ -0,0 +1,7 @@
+var fs = require('fs');
+
+consol.log('genEnv is Incomplete');
+// Read HarperDB/install_log.log
+// Parse for Username & Password
+// Create .env file
+// Set Env variables HARPERDB_USERNAME && HARPERDB_PASSWORD
