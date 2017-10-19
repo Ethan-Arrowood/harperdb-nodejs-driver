80ac3a5a4b5ea15b2137961af7259ab08892cb0f
Andrew DeChristopher <andrewdechristopher@gmail.com>
2017-10-19 01:06:09 -0400

    Implemented event emission. Documentation needed.

--- HarperDB.js
+++ HarperDB.js
@@ -1,13 +1,17 @@
+// Core modules
 var http = require("http");
-var request = require('request');
+var request = require("request");
 var base64 = require("base-64");
-var rp = require('request-promise');
+var rp = require("request-promise");
 
+// Event module
+var events = require("events");
 
 class HarperDB {
   constructor() {
     this.options = {};
     this.isConnected = false;
+    this.event = new events.EventEmitter();
   }
 
   connect(url, username, password) {
@@ -17,34 +21,39 @@ class HarperDB {
     const authorization = `Basic ${base64.encode(`${username}:${password}`)}`;
 
     const options = {
-      "method": "POST",
+      method: "POST",
       url,
-      "headers": {
+      headers: {
         "content-type": "application/json",
         authorization
       },
-      "json": true,
-    }
+      json: true
+    };
 
     rp({
       ...options,
-      body: {"operation": "list_users"}
+      body: { operation: "list_users" }
     })
       .then(() => {
+        this.event.emit("connection");
         console.log("Succesfully connected to server!");
         this.isConnected = true;
         this.options = options;
       })
       .catch(error => {
+        this.event.emit("error");
         console.log(`Error: ${error.message}`);
-      })
+      });
   }
 
   request(query) {
-    if(!this.isConnected)
+    if (!this.isConnected) {
+      this.event.emit("error");
       throw new Error("Must connect to DB first using .connect()");
-    else
-      return rp({...this.options, body: query });
+    } else {
+      this.event.emit("request");
+      return rp({ ...this.options, body: query });
+    }
   }
 }
 
